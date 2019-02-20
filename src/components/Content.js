167d1d14c73d3e38e0220ed4db24b5d225a4400a
stizzle123 <arifayanidowu2@yahoo.com>
2019-02-20 20:11:42 +0100

    Modified Header.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,84 +1,116 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "../img/logo.png";
 import face6 from "../img/face6.jpeg";
 
-export default () => {
-  return (
-    <div class="content">
-      <div class="middle__top">
-        <div class="middle__top-heading">
-          <div class="middle__top-heading__info">
-            <img src={logo} alt="Logo" class="middle__top-heading__avatar" />
-            <div class="middle__top-heading__handle">
-              <h1>New DM Feature</h1>
-              <p>Sept. 24, 2018, 1:16 PM</p>
+class Content extends Component {
+  state = {
+    likes1: 101,
+    likes2: 106,
+    views: 366
+  };
+
+  addLikes1 = () => {
+    this.setState({
+      likes1: this.state.likes1 + 1
+    });
+  };
+
+  addLikes2 = () => {
+    this.setState({
+      likes2: this.state.likes2 + 1
+    });
+  };
+
+  addViews = () => {
+    this.setState({
+      views: this.state.views + 1
+    });
+  };
+
+  render() {
+    return (
+      <div class="content">
+        <div class="middle__top">
+          <div class="middle__top-heading">
+            <div class="middle__top-heading__info">
+              <img src={logo} alt="Logo" class="middle__top-heading__avatar" />
+              <div class="middle__top-heading__handle">
+                <h1>New DM Feature</h1>
+                <p>Sept. 24, 2018, 1:16 PM</p>
+              </div>
+            </div>
+            <div class="middle__top-heading__icon">
+              <i class="fas fa-ellipsis-h" />
             </div>
           </div>
-          <div class="middle__top-heading__icon">
-            <i class="fas fa-ellipsis-h" />
+
+          <div class="middle__top-content">
+            <p>
+              Hello Guys, <br />
+              <br />
+              <br />
+              We'd like to say a big thank you to all those who took part in our
+              survey &mdash; with the feedback you gave us, we are better
+              equipped to take TwoCents to a new sense of what the product
+              should morph into; we have learned a lot and have a pretty good
+              understanding of... <span class="read-more">Read More</span>
+            </p>
+          </div>
+          <div class="middle__top-content__footer">
+            <i class="fas fa-thumbs-up" onClick={this.addLikes1} />{" "}
+            {this.state.likes1}
           </div>
         </div>
 
-        <div class="middle__top-content">
-          <p>
-            Hello Guys, <br />
-            <br />
-            <br />
-            We'd like to say a big thank you to all those who took part in our
-            survey &mdash; with the feedback you gave us, we are better equipped
-            to take TwoCents to a new sense of what the product should morph
-            into; we have learned a lot and have a pretty good understanding
-            of... <span class="read-more">Read More</span>
-          </p>
-        </div>
-        <div class="middle__top-content__footer">
-          <i class="fas fa-thumbs-up" /> 101
-        </div>
-      </div>
-
-      <div class="middle__bottom">
-        <div class="middle__bottom-heading">
-          <div class="middle__bottom-heading__info">
-            <img
-              src={face6}
-              alt="Logo"
-              class="middle__bottom-heading__avatar"
-            />
-            <div class="middle__bottom-heading__handle">
-              <h1>
-                Yele Bademosi <span>E</span>&nbsp; <small>Jan 2, 8:31 PM</small>{" "}
-              </h1>
-              <p>Angel Investor @Microtaction</p>
+        <div class="middle__bottom">
+          <div class="middle__bottom-heading">
+            <div class="middle__bottom-heading__info">
+              <img
+                src={face6}
+                alt="Logo"
+                class="middle__bottom-heading__avatar"
+              />
+              <div class="middle__bottom-heading__handle">
+                <h1>
+                  Yele Bademosi <span>E</span>&nbsp;{" "}
+                  <small>Jan 2, 8:31 PM</small>{" "}
+                </h1>
+                <p>Angel Investor @Microtaction</p>
+              </div>
+            </div>
+            <div class="middle__bottom-heading__icon">
+              <i class="fas fa-ellipsis-h" />
             </div>
           </div>
-          <div class="middle__bottom-heading__icon">
-            <i class="fas fa-ellipsis-h" />
+
+          <div class="middle__bottom-content">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi vero
+              repellendus velit labore voluptatum perferendis similique minima,
+              corporis exercitationem sequi corrupti soluta harum aspernatur,
+              distinctio debitis possimus accusamus id eius tempore, beatae
+              asperiores illo. Quisquam, non harum. Porro, eum eaque!...
+              <span class="read-more">Read More</span>
+            </p>
+          </div>
+
+          <div class="middle__bottom-content__footer">
+            <span>
+              <i class="fas fa-thumbs-up" onClick={this.addLikes2} />{" "}
+              {this.state.likes2}
+            </span>
+            <span>
+              <i class="fas fa-eye" onClick={this.addViews} />{" "}
+              {this.state.views}
+            </span>
+            <span>
+              <i class="fas fa-comment-alt" /> 11
+            </span>
           </div>
         </div>
-
-        <div class="middle__bottom-content">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi vero
-            repellendus velit labore voluptatum perferendis similique minima,
-            corporis exercitationem sequi corrupti soluta harum aspernatur,
-            distinctio debitis possimus accusamus id eius tempore, beatae
-            asperiores illo. Quisquam, non harum. Porro, eum eaque!...
-            <span class="read-more">Read More</span>
-          </p>
-        </div>
-
-        <div class="middle__bottom-content__footer">
-          <span>
-            <i class="fas fa-thumbs-up" /> 103
-          </span>
-          <span>
-            <i class="fas fa-eye" /> 366
-          </span>
-          <span>
-            <i class="fas fa-comment-alt" /> 11
-          </span>
-        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+export default Content;
--- src/components/Header.js
+++ src/components/Header.js
@@ -11,6 +11,26 @@ class Header extends Component {
     hover.classList.toggle("show");
   };
 
+  onShowDropdown2 = _ => {
+    const hover = document.querySelector(".hover2");
+    hover.classList.toggle("show");
+  };
+
+  onShowDropdown3 = _ => {
+    const hover = document.querySelector(".hover3");
+    hover.classList.toggle("show");
+  };
+
+  addOverlay = () => {
+    const overlay = document.querySelector(".overlay");
+    overlay.classList.add("show");
+  };
+
+  removeOverlay = () => {
+    const overlay = document.querySelector(".overlay");
+    overlay.classList.remove("show");
+  };
+
   render() {
     return (
       <div className="header">
@@ -45,8 +65,8 @@ class Header extends Component {
         <div className="header__notifications">
           <div className="header__notifications-wrapper">
             <i
-              className="fas fa-envelope header__notifications--icon"
-              onClick={this.onShowDropdown}
+              className="fas  fa-envelope header__notifications--icon"
+              onMouseOver={this.onShowDropdown}
             />
             <div className="header__notifications-dropdown hover">
               <p>View Messages</p>
@@ -93,14 +113,170 @@ class Header extends Component {
               </ul>
             </div>
           </div>
-          <i className="fas fa-bell header__notifications--icon" />
-          <img
-            src={face1}
-            alt="Profile Pic"
-            className="header__notifications--avatar"
-          />
+          <div className="header__notifications-wrapper">
+            <i
+              className="fas fa-bell header__notifications--icon"
+              onMouseOver={this.onShowDropdown2}
+            />
+            <div className="header__notifications-dropdown hover2">
+              <ul className="header__notifications-dropdown__content">
+                <li className="header__notifications-dropdown__items">
+                  <div className="header__notifications-dropdown__info">
+                    <div className="header__notifications-dropdown__avatar">
+                      <i class="fas fa-user" />
+                    </div>
+                    <div className="header__notifications-dropdown__message">
+                      <h3>
+                        Eva Ngugi <span>shared and Insight</span>
+                      </h3>
+                      <p>1 min(s) ago</p>
+                    </div>
+                  </div>
+                </li>
+                <li className="header__notifications-dropdown__items">
+                  <div className="header__notifications-dropdown__info">
+                    <div className="header__notifications-dropdown__avatar">
+                      <i class="fas fa-user" />
+                    </div>
+                    <div className="header__notifications-dropdown__message">
+                      <h3>
+                        Kemi Leo and Dolaris <span>are now following you</span>
+                      </h3>
+                      <p>2 min(s) ago</p>
+                    </div>
+                  </div>
+                </li>
+                <li className="header__notifications-dropdown__items">
+                  <div className="header__notifications-dropdown__info">
+                    <div className="header__notifications-dropdown__avatar">
+                      <p>Tc</p>
+                    </div>
+                    <div className="header__notifications-dropdown__message">
+                      <h3>
+                        TwoCents <span>shared a news post</span>
+                      </h3>
+                      <p>3 min(s) ago</p>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="header__notifications-wrapper">
+            <img
+              src={face1}
+              alt="Profile Pic"
+              className="header__notifications--avatar"
+              onMouseOver={this.onShowDropdown3}
+            />
+            <div className="header__notifications--avatar__dropdown hover3">
+              <div className="header__notifications--avatar__dropdown-top">
+                <ul className="header__notifications--avatar__list">
+                  <li className="header__notifications--avatar__item">
+                    <i class="fas fa-user" /> Profile
+                  </li>
+                  <li className="header__notifications--avatar__item">
+                    <i class="fas fa-envelope" /> Messages
+                  </li>
+                  <li className="header__notifications--avatar__item">
+                    <i class="fas fa-cog" /> Settings
+                  </li>
+                </ul>
+              </div>
+
+              <div className="header__notifications--avatar__dropdown-middle">
+                <ul className="header__notifications--avatar__list">
+                  <li className="header__notifications--avatar__item">
+                    <i class="fas fa-info-circle" /> About
+                  </li>
+                  <li className="header__notifications--avatar__item">
+                    <i class="far fa-newspaper" /> News
+                  </li>
+                  <li className="header__notifications--avatar__item">
+                    <i class="fas fa-question-circle" /> FAQs
+                  </li>
+                  <li className="header__notifications--avatar__item">
+                    <i class="fas fa-phone-square" /> Contact
+                  </li>
+                  <li className="header__notifications--avatar__item">
+                    <i class="fas fa-file-alt" /> Legal
+                  </li>
+                </ul>
+              </div>
+
+              <div className="header__notifications--avatar__dropdown-bottom">
+                <ul className="header__notifications--avatar__list">
+                  <li className="header__notifications--avatar__item">
+                    <i class="fas fa-sign-out-alt" /> Sign Out
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
-        <button className="header__post">New Post</button>
+        <button className="header__post" onClick={this.addOverlay}>
+          New Post
+        </button>
+        <div className="overlay">
+          <div className="overlay-content">
+            <div className="overlay-close" onClick={this.removeOverlay}>
+              <i class="fas fa-times" />
+            </div>
+            <div className="overlay-content__header">
+              <ul>
+                <li>
+                  <Link to="/" className="active">
+                    Question
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/">Insight</Link>
+                </li>
+              </ul>
+            </div>
+            <form>
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="topic"
+                  id="topic"
+                  placeholder="Topic"
+                />
+                <label htmlFor="topic" className="form-label">
+                  Topic
+                </label>
+              </div>
+              <div className="form-group">
+                <textarea
+                  type="text"
+                  name="question"
+                  id="question"
+                  placeholder="Type your question"
+                />
+                <label htmlFor="question" className="form-label">
+                  Type your question
+                </label>
+                <p className="float-right">47/200</p>
+              </div>
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="mention"
+                  id="mention"
+                  placeholder="@mention someone to ask for an answer"
+                />
+                <label htmlFor="mention" className="form-label">
+                  @mention someone to ask for an answer
+                </label>
+              </div>
+              <div className="form-group mb-3">
+                <button className="btn-green" type="submit">
+                  POST
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
